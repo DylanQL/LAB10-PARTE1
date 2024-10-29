386dc1411a2331e620cdc05bf67dfd4ae4cab5ee
DylanQL <angelo.quispe.l@tecsup.edu.pe>
2024-10-29 01:02:01 -0500

    commit

---
+++ src/main/java/pe/edu/tecsup/Calculator.java
@@ -0,0 +1,16 @@
+package pe.edu.tecsup;
+
+public class Calculator {
+
+
+    public int sub(int i, int j) {
+        return i - j;
+    }
+
+
+    public int add(int i, int j) {
+        return i + j;
+
+
+    }
+}
